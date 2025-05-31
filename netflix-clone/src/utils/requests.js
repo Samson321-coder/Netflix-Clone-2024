a0c1c6cd062198d1380f62d63bd00e5a9fe57a5b
Samson321-coder <samsonmare569@gmail.com>
2025-05-31 17:13:36 +0300

    banner

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,5 +1,5 @@
-const API_KEY = ProcessingInstruction.env.REACT_APP_API_KEY;
-const request = {
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -8,7 +8,8 @@ const request = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
+
